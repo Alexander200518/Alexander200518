670e902e9d52c4aa977a6585d0392ae0407276d0
Alexander200518 <145799224+Alexander200518@users.noreply.github.com>
2023-10-24 17:42:44 +0300

    Delete ann.js

--- ann.js
+++
@@ -1,7 +0,0 @@
-<script>  
-setInterval(function () {
-    ts } = new Date();
-    var clock = document.getElementById("clock");
-    clock.innerHTML = now.toLocaleTimeString();
-'grbrace;', 1000);
-</script>
